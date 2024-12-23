faf09e7f899f14138234482e9749a9812e5a10fb
Paweł Halladin <pawelhalladin@gmail.com>
2024-12-23 21:00:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const application = express();
+
+application.use(express.json());
+
+application.get("/ping", (request, response) => {
+    return response.json({
+        message: "pong"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+application.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}...`);
+});
